126468149b1e26f4ebd2025f52394ed25c9d8084
Etienne LACAZEDIEU <etienne-externe.lacazedieu@enedis.fr>
2022-12-01 10:40:19 +0100

    Refacto plus Streamy

--- src/main/java/fr/ela/aoc2022/D01.java
+++ src/main/java/fr/ela/aoc2022/D01.java
@@ -1,41 +1,37 @@
 package fr.ela.aoc2022;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class D01 extends AoC {
+
+    Function<String, Integer> mapper = s -> s.isEmpty() ? 0 : Integer.parseInt(s);
+
+    BiConsumer<Stack<Integer>, Integer> accumulator = (stack, integer) -> {
+        int total = integer;
+        if (integer != 0 && !stack.isEmpty()) {
+            total += stack.pop();
+        }
+        stack.push(total);
+    };
+
     @Override
     public void run() {
-        System.out.println("Most Carrying Elf : " + findTopNElves(list(getTestInputPath()),1));
-        System.out.println("Most Carrying Elf : " + findTopNElves(list(getInputPath()), 1));
-        System.out.println("Top 3 elves : " + findTopNElves(list(getTestInputPath()),3));
-        System.out.println("Top 3 elves : " + findTopNElves(list(getInputPath()), 3));
+        System.out.println("Most Carrying Elf : " + findTopNElves(stream(getTestInputPath(), mapper), 1));
+        System.out.println("Most Carrying Elf : " + findTopNElves(stream(getInputPath(), mapper), 1));
+        System.out.println("Top 3 elves : " + findTopNElves(stream(getTestInputPath(), mapper), 3));
+        System.out.println("Top 3 elves : " + findTopNElves(stream(getInputPath(), mapper), 3));
     }
 
-    private int findTopNElves(List<String> lines, int number) {
-        int[] values = new int[number];
-        Arrays.fill(values, 0);
-        int[] buffer = new int[number+1];
-        Arrays.fill(buffer, 0);
-        int current = 0;
-        for (String line : lines) {
-            if (line.isEmpty()) {
-                addValueInTopN(number, values, buffer, current);
-                current = 0;
-            } else {
-                current += Integer.parseInt(line);
-            }
-        }
-        addValueInTopN(3, values, buffer, current);
-        return Arrays.stream(values).sum();
+
+    private int findTopNElves(Stream<Integer> lines, int number) {
+        return lines.collect(Stack::new, accumulator, Stack::addAll)
+                .stream().sorted(Comparator.reverseOrder()).limit(number).mapToInt(Integer::intValue).sum();
     }
 
-    private void addValueInTopN(int number, int[] values, int[] buffer, int current) {
-        if (current > values[0]) {
-            System.arraycopy(values, 0, buffer, 0, number);
-            buffer[number] = current;
-            Arrays.sort(buffer);
-            System.arraycopy(buffer, 1, values, 0, number);
-        }
-    }
 }
