dfbafb80f568ba506fa4019cb459c008eaad526e
Etienne LACAZEDIEU <etienne-externe.lacazedieu@enedis.fr>
2022-12-02 15:31:59 +0100

    Day 2 : il y avait plus simple

--- src/main/java/fr/ela/aoc2022/D02.java
+++ src/main/java/fr/ela/aoc2022/D02.java
@@ -1,10 +1,10 @@
 package fr.ela.aoc2022;
 
+import java.util.function.ToIntBiFunction;
 import java.util.function.ToIntFunction;
 import java.util.stream.IntStream;
 
 public class D02 extends AoC {
-
     public static final int ROCK = 0;
     public static final int PAPER = 1;
     public static final int SCISSORS = 2;
@@ -13,45 +13,71 @@ public class D02 extends AoC {
     public static final int DRAW = 1;
     public static final int WIN = 2;
 
-    static int score(int play, int outcome) {
-        return play + 1 + outcome * 3;
-    }
+    record IntPair(int one, int two) {}
 
-    ToIntFunction<String> scorePartOne = (line) -> {
+    static IntPair toPair(String line) {
         int one = line.charAt(0) - 'A';
         int two = line.charAt(2) - 'X';
-        return score(two, outcome(one, two));
-    };
+        return new IntPair(one, two);
+    }
 
-    ToIntFunction<String> scorePartTwo = (line) -> {
-        int one = line.charAt(0) - 'A';
-        int outcome = line.charAt(2) - 'X';
-        return score(whatToPlay(one, outcome), outcome);
-    };
-
-    public static int outcome(int one, int two) {
-        if (one == two) {
+    /**
+     * Calcule le résultat en fonction des 2 coups joués;
+     * @param pair one = adversaire, two = moi
+     * @return le résultat.
+     */
+    public static int outcome(IntPair pair) {
+        if (pair.one == pair.two) {
             return DRAW;
         }
-        return switch (one) {
-            case ROCK -> two == PAPER ? WIN : LOSE;
-            case SCISSORS -> two == ROCK ? WIN : LOSE;
-            case PAPER -> two == SCISSORS ? WIN : LOSE;
-            default -> throw new IllegalArgumentException(one + " : invalid play value");
+        return switch (pair.one) {
+            case ROCK -> pair.two == PAPER ? WIN : LOSE;
+            case SCISSORS -> pair.two == ROCK ? WIN : LOSE;
+            case PAPER -> pair.two == SCISSORS ? WIN : LOSE;
+            default -> throw new IllegalArgumentException(pair.one + " : invalid play value");
         };
     }
 
-    public static int whatToPlay(int one, int outcome) {
-        return IntStream.range(0, 3).filter(two -> outcome(one, two) == outcome).findFirst().orElseThrow();
+    /**
+     * Vazy, en fait c'est un module.
+     * @param pair
+     * @return
+     */
+    public static int smartOutcome(IntPair pair) {
+        return ((pair.two - pair.one) + 4) % 3;
     }
 
+    /** Inverse de fonction comme un goret :)
+     * @param pair one = le coup joué, two = le résultat
+     * @return le coup à jouer pour obtenir le résultat.
+     */
+    public static int whatToPlay(final IntPair pair) {
+        return IntStream.range(0, 3).filter(two -> outcome(new IntPair(pair.one, two)) == pair.two).findFirst().orElseThrow();
+    }
+
+    /**
+     * Là aussi c'est un modulo :)
+     * @param pair
+     * @return
+     */
+    public static int smartWhatToPlay(IntPair pair) {
+        return (pair.one + pair.two + 2) % 3;
+    }
+
+    // Scoring function : what I played = one, outcome = two.
+    ToIntFunction<IntPair> scoring = pair -> pair.one + 1 + pair.two * 3;
+
+    ToIntBiFunction<IntPair, ToIntFunction<IntPair>> partOne = (pair, outcomeFunction) -> scoring.applyAsInt(new IntPair(pair.two, outcomeFunction.applyAsInt(pair)));
+    ToIntBiFunction<IntPair, ToIntFunction<IntPair>> partTwo = (pair, whatToPlayFunction) -> scoring.applyAsInt(new IntPair(whatToPlayFunction.applyAsInt(pair), pair.two));
 
     @Override
     public void run() {
-        System.out.println("Test Score : " + stream(getTestInputPath()).mapToInt(scorePartOne).sum());
-        System.out.println("Real Score : " + stream(getInputPath()).mapToInt(scorePartOne).sum());
-        System.out.println("Test Score : " + stream(getTestInputPath()).mapToInt(scorePartTwo).sum());
-        System.out.println("Real Score : " + stream(getInputPath()).mapToInt(scorePartTwo).sum());
+        System.out.println("Test Score : " + stream(getTestInputPath(), D02::toPair).mapToInt(pair -> partOne.applyAsInt(pair, D02::outcome)).sum());
+        System.out.println("Real Score : " + stream(getInputPath(), D02::toPair).mapToInt(pair -> partOne.applyAsInt(pair, D02::outcome)).sum());
+        System.out.println("Real Smart Score : " + stream(getInputPath(), D02::toPair).mapToInt(pair -> partOne.applyAsInt(pair, D02::smartOutcome)).sum());
+        System.out.println("Test Score : " + stream(getTestInputPath(), D02::toPair).mapToInt(pair-> partTwo.applyAsInt(pair, D02::whatToPlay)).sum());
+        System.out.println("Real Score : " + stream(getInputPath(), D02::toPair).mapToInt(pair-> partTwo.applyAsInt(pair, D02::whatToPlay)).sum());
+        System.out.println("Real Smart Score : " + stream(getInputPath(), D02::toPair).mapToInt(pair-> partTwo.applyAsInt(pair, D02::smartWhatToPlay)).sum());
     }
 
 
