ec0168caa6d8b7ad4a1b5e5a83ce8038f95f94dc
e.lacazedieu <etienne.lacazedieu@gmail.com>
2022-12-21 15:16:38 +0100

    D21 , part 2 : very FUNCTIONS! :)

--- src/main/java/fr/ela/aoc2022/D21.java
+++ src/main/java/fr/ela/aoc2022/D21.java
@@ -3,11 +3,12 @@ package fr.ela.aoc2022;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class D21 extends AoC {
 
     private class MonkeyMap {
-        Map<String, OpMonkey> unresolved = new HashMap<>();
+        Map<String, UnresolvedMonkey> unresolved = new HashMap<>();
         Map<String, NumberMonkey> resolved = new HashMap<>();
 
         public MonkeyMap combine(MonkeyMap other) {
@@ -20,8 +21,8 @@ public class D21 extends AoC {
             if (monkey instanceof NumberMonkey) {
                 resolved.put(monkey.name(), (NumberMonkey) monkey);
             }
-            if (monkey instanceof OpMonkey) {
-                unresolved.put(monkey.name(), (OpMonkey) monkey);
+            if (monkey instanceof UnresolvedMonkey) {
+                unresolved.put(monkey.name(), (UnresolvedMonkey) monkey);
             }
         }
 
@@ -29,20 +30,23 @@ public class D21 extends AoC {
             return resolved.containsKey(name);
         }
 
-        void resolve() {
+        boolean resolve() {
             while (!unresolved.isEmpty()) {
-                List<String> calculated = new ArrayList<>();
-                for (Map.Entry<String, OpMonkey> current : unresolved.entrySet()) {
-                    String name = current.getKey();
-                    OpMonkey monkey = current.getValue();
-                    if (monkey.dependsOn().stream().allMatch(this::isResolved)) {
-                        calculated.add(name);
-                        NumberMonkey nm = monkey.resolve(resolved);
-                        resolved.put(nm.name, nm);
+                List<Monkey> calculated = new ArrayList<>();
+                for (Map.Entry<String, UnresolvedMonkey> current : unresolved.entrySet()) {
+                    UnresolvedMonkey monkey = current.getValue();
+                    if (monkey.dependsOn().stream().anyMatch(this::isResolved)) {
+                        Monkey nm = monkey.resolve(resolved);
+                        calculated.add(nm);
+                        add(nm);
                     }
                 }
-                calculated.forEach(unresolved::remove);
+                if (calculated.isEmpty()) {
+                    return false;
+                }
+                calculated.stream().filter(m -> m instanceof NumberMonkey).map(Monkey::name).forEach(unresolved::remove);
             }
+            return true;
         }
     }
 
@@ -51,18 +55,56 @@ public class D21 extends AoC {
 
     }
 
+    public interface UnresolvedMonkey extends Monkey {
+        String name();
+
+        Monkey resolve(Map<String, NumberMonkey> monkeyMap);
+
+        List<String> dependsOn();
+    }
+
     public record NumberMonkey(String name, long value) implements Monkey {
     }
 
-    public record OpMonkey(String name, BiFunction<Long, Long, Long> operation, String left,
-                           String right) implements Monkey {
+    public record FunctionMonkey(String name, Function<Long, Long> operation, Function<Long, Long> inverse,
+                                 long value,
+                                 String operand) implements UnresolvedMonkey {
+        public List<String> dependsOn() {
+            return List.of(operand);
+        }
+
+        public NumberMonkey resolve(Map<String, NumberMonkey> monkeyMap) {
+            return new NumberMonkey(name, operation.apply(monkeyMap.get(operand).value()));
+        }
+    }
+
+    public record BiFunctionMonkey(String name,
+                                   BiFunction<Long, Long, Long> operation,
+                                   BiFunction<Long, Long, Long> leftInverse,
+                                   BiFunction<Long, Long, Long> rightInverse,
+                                   String left,
+                                   String right) implements UnresolvedMonkey {
 
         public List<String> dependsOn() {
             return List.of(left, right);
         }
 
-        public NumberMonkey resolve(Map<String, NumberMonkey> monkeyMap) {
-            return new NumberMonkey(name, operation.apply(monkeyMap.get(left).value(), monkeyMap.get(right).value()));
+
+        public Monkey resolve(Map<String, NumberMonkey> monkeyMap) {
+            NumberMonkey leftMonkey = monkeyMap.get(left);
+            NumberMonkey rightMonkey = monkeyMap.get(right);
+            if (leftMonkey != null && rightMonkey != null) {
+                return new NumberMonkey(name, operation.apply(leftMonkey.value, rightMonkey.value));
+            }
+            if (leftMonkey == null) {
+                Function<Long, Long> f = l -> operation.apply(l, rightMonkey.value);
+                Function<Long, Long> inverse = x -> leftInverse.apply(x, rightMonkey.value);
+                return new FunctionMonkey(name, f, inverse, rightMonkey.value, left);
+            } else {
+                Function<Long, Long> f = r -> operation.apply(leftMonkey.value, r);
+                Function<Long, Long> inverse = x -> rightInverse.apply(x, leftMonkey.value);
+                return new FunctionMonkey(name, f, inverse, leftMonkey.value, right);
+            }
         }
     }
 
@@ -71,21 +113,18 @@ public class D21 extends AoC {
         String name = parts[0];
         String[] val = parts[1].trim().split(" ");
         if (val.length == 1) {
-            //dbpl: 5
             long value = Long.parseLong(val[0]);
             return new NumberMonkey(name, value);
         }
-        //cczh: sllz + lgvd
         String left = val[0];
         String right = val[2];
-        BiFunction<Long, Long, Long> operation = switch (val[1].charAt(0)) {
-            case '+' -> (a,b) -> a+b;
-            case '*' -> (a,b) -> a*b;
-            case '-' -> (a,b) -> a-b;
-            case '/' -> (a,b) -> a/b;
+        return switch (val[1].charAt(0)) {
+            case '+' -> new BiFunctionMonkey(name, (l, r) -> l + r, (x, r) -> x - r, (x, l) -> x - l, left, right);
+            case '*' -> new BiFunctionMonkey(name, (l, r) -> l * r, (x, r) -> x / r, (x, l) -> x / l, left, right);
+            case '-' -> new BiFunctionMonkey(name, (l, r) -> l - r, (x, r) -> x + r, (x, l) -> l - x, left, right);
+            case '/' -> new BiFunctionMonkey(name, (l, r) -> l / r, (x, r) -> x * r, (x, l) -> l / x, left, right);
             default -> throw new IllegalStateException();
         };
-        return new OpMonkey(name, operation, left, right);
     }
 
     public MonkeyMap parse(Path path) {
@@ -96,10 +135,28 @@ public class D21 extends AoC {
     public void partOne(String kind, Path path) {
         MonkeyMap map = parse(path);
         map.resolve();
-        System.out.println(kind+" result part one : "+map.resolved.get("root").value);
+        System.out.println(kind + " result part one : " + map.resolved.get("root").value);
     }
 
-    public void partTwo(String kind) {
+    public void partTwo(String kind, Path path) {
+        MonkeyMap monkeyMap = parse(path);
+        // Get root and human.
+        monkeyMap.resolved.remove("humn");
+        monkeyMap.resolve();
+
+        FunctionMonkey root = (FunctionMonkey) monkeyMap.unresolved.get("root");
+        long value = root.value;
+
+        FunctionMonkey r = (FunctionMonkey) monkeyMap.unresolved.get(root.operand);
+        Function<Long, Long> function = Function.identity();
+        Function<Long, Long> f2 = Function.identity();
+        do {
+            function = function.andThen(r.inverse);
+            r = (FunctionMonkey) monkeyMap.unresolved.get(r.operand);
+        } while (! "humn".equals(r.operand));
+        function = function.andThen(r.inverse);
+        System.out.println(kind + " result part two : " + function.apply(value));
+
     }
 
     @Override
@@ -107,8 +164,9 @@ public class D21 extends AoC {
         partOne("Test", getTestInputPath());
         partOne("Real", getInputPath());
 
-        //partTwo("Test");
-        //partTwo("Real");
+        partTwo("Test", getTestInputPath());
+        partTwo("Real", getInputPath());
+
     }
 
 }
